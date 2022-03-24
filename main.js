b67d32c9525606e664046ff61d70798bd120cbc6
Ritvik2379 <86002788+Ritvik2379@users.noreply.github.com>
2022-03-24 17:43:27 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function setup(){
+    video=createCapture(VIDEO);
+    video.size(550,500);
+    canvas=createCanvas(550,550);
+    canvas.position(570,100);
+    posenet=ml5.poseNet(video,modelLoaded);
+    posenet.on('pose',gotPoses);
+}
+function modelLoaded(){
+    console.log("Posenet is initialized");
+}
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
+    }
+}    
